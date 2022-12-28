284771fe0f113696d3ce150c54e44f200fb1c029
rahmou1 <ahmedrahmou11694@gmail.com>
2022-12-28 10:55:32 +0200

    adding timer, remove, add, useeffect

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value; // always a string even the input is a number so we need to change it to number
+    const enteredAmountNumber = +enteredAmount; // changed to a number
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id, // this changed!
@@ -16,6 +36,8 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+
+      {!amountIsValid && <p>Please select a valid Value (1-5) </p>}
     </form>
   );
 };
