b5c2a2035fd0ea4895eabe1d1665b6d8f10e1ec4
rahmou1 <ahmedrahmou11694@gmail.com>
2022-12-28 06:57:25 +0200

    insert header and mapped some meals

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Rahmou Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img alt="table of foods" src={mealsImage} />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
